49ec690d9581a79bc7e10b3005aaee3a5ad7cd3e
moraaa24 <mercymoraa705@gmail.com>
2023-08-15 10:02:37 -0700

    a program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+int _puchar(char c);
+int main(void) {
+	char str[] = "-puchar\n";
+
+	for (int i = 0; str[i] ! = '\o'; i++) {
+		-puchar(str[i]);
+
+		{
+			return 0;
+	}
+	int -putchar(char c) {
+	
+	return c;
+	
+	}
