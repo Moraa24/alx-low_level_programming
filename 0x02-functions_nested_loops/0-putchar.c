e96a7743514f3348d24f17fa3023e18010cf2f5d
moraaa24 <mercymoraa705@gmail.com>
2023-08-15 10:36:50 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,6 @@
-int _putchar(char c);
+#include <stdio.h>
 
-int main(void) {
-    char str[] = "_putchar\n";
-
-    for (int i = 0; str[i] != '\0'; i++) {
-        _putchar(str[i]);
-    }
-
+int main() {
+    printf("_putchar\n");
     return 0;
-}
-
-int _putchar(char c) {
-    // Code to output the character 'c'
-    // You would need to replace this with the actual mechanism
-    // for outputting a character in your environment
-    return c; // Return the character as an integer
 }
