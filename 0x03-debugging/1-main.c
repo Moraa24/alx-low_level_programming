e6259530488312e19c0ba62eccaea93383f1202a
moraaa24 <mercymoraa705@gmail.com>
2023-08-16 13:26:42 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -15,7 +15,6 @@ printf("Infinite loop incoming :(\n");
 i = 0;
 
 /*while (i < 10)*/
-
 /*{*/
 /*putchar(i);*/
 /*}*/
@@ -23,5 +22,4 @@ i = 0;
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
-
-{
+}
