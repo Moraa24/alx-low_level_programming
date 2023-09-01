d498a0637567b68c61d2bccb373d2c9019c22ced
moraaa24 <mercymoraa705@gmail.com>
2023-09-01 14:08:22 -0700

    0-whatsmyname.c

--- 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
+++ 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
- * @argc: The number of command-line arguments
- * @argv: An array of strings containing the arguments
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
-
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-    if (argc > 0)
-    {
-        printf("%s\n", argv[0]);
-    }
-    return (0);
+	printf("%s\n", *argv);
+
+	return (0);
 }
